15b1f9c0c1f2904e134e242ec9e6b22eeeac11dc
Harry Quan <harryquanx@gmail.com>
2025-09-23 17:35:02 +1200

    simple back end server configured with mDNS registration for dynamic ip address

--- backend/main.py
+++ backend/main.py
@@ -4,23 +4,37 @@ from zeroconf import Zeroconf, ServiceInfo
 import socket
 import threading
 
+from register_mdns_service import register_mdns_service
+
 app = FastAPI()
 
-clients = []
+# clients = []
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    clients.append(websocket)
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     clients.append(websocket)
 
-    try:
-        while True:
-            data = await websocket.receive_text()
-            print(f"Message received from ESP32: {data}")
+#     try:
+#         while True:
+#             data = await websocket.receive_text()
+#             print(f"Message received from ESP32: {data}")
 
-    except WebSocketDisconnect:
-        clients.remove(websocket)
-        print("Client disconnected")
+#     except WebSocketDisconnect:
+#         clients.remove(websocket)
+#         print("Client disconnected")
+
+# ========== FastAPI Routes ==========
+@app.get("/hello")
+async def hello():
+    return {"message": "Hello from FastAPI via mDNS!"}
 
 if __name__ == "__main__":
+
+    zeroconf_instance, service_info = register_mdns_service(
+        service_name="backend server._http._tcp.local.",
+        service_type="_http._tcp.local.",
+        port=8000
+    )
+
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
