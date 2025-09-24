7ace13aedfd97c20ffc4b1b60af61639460f2ed2
Harry Quan <harryquanx@gmail.com>
2025-09-24 13:40:34 +1200

    try to reconnect to server if failed

--- esp32/light_sensor/light_sensor.ino
+++ esp32/light_sensor/light_sensor.ino
@@ -5,23 +5,36 @@
 #include <ArduinoWebsockets.h>
 
 const char* ssid = "ESP32";  // AP network name
-const char* password = "";    // No password
+const char* password = "";    // password
 const int ldrPin = 34;        // LDR ADC pin
 
 const char* serverIP = "esp32-backend.local";
 const int serverPort = 8000;
 const char* wsUri = "/ws";
 
-using namespace websockets; //WebsocketsClient class
+// AsyncWebServer server(80);    // HTTP server on port 80
+// AsyncWebSocket ws("/ws");     // WebSocket handler
 
-AsyncWebServer server(80);    // HTTP server on port 80
-AsyncWebSocket ws("/ws");     // WebSocket handler
+using namespace websockets; 
 WebsocketsClient client;
 
 // Static IP configuration for SoftAP
 IPAddress local_IP(192, 168, 4, 1);  // Fixed IP address for ESP32 in AP mode
 IPAddress gateway(192, 168, 4, 1);   // Gateway is the same as the local IP
 IPAddress subnet(255, 255, 255, 0);  // Subnet mask
+
+void connectToWebSocketServer(){
+  String wsUrl = String("ws://") + serverIP + ":" + String(serverPort) + wsUri;   // String wsUrl = String("ws://esp32-backend.local:8000/ws");
+  while (!client.connect(wsUrl)) {   // Connect to WebSocket server
+    Serial.println("Trying to connect to WebSocket server!");
+    delay(1000);
+  }
+  Serial.println("Connected to WebSocket server!");
+}
+
+void onMessageCallBack(WebsocketsMessage msg){
+  Serial.println("Mesasge received: " + msg.data());
+} 
 
 void setup() {
   Serial.begin(115200);
@@ -37,44 +50,26 @@ void setup() {
     delay(1000);
   }
 
-  String wsUrl = String("ws://") + serverIP + ":" + String(serverPort) + wsUri;
-  // String wsUrl = String("ws://esp32-backend.local:8000/ws");
-  while (!client.connect(wsUrl)) {   // Connect to WebSocket server
-    Serial.println("Trying to connect to WebSocket server!");
-    delay(1000);
-  }
-  Serial.println("Connected to WebSocket server!");
+  // websocket config
+  client.onMessage(onMessageCallBack);
+  connectToWebSocketServer();
 
-
-  // Handle WebSocket events (on connect, disconnect)
-  // ws.onEvent(onWsEvent);  // Make sure the function signature matches the expected one
- 
-  server.addHandler(&ws);
-  server.begin();   // Start HTTP server (optional, just for general HTTP routes)
+  // server.addHandler(&ws);
+  // server.begin();   // Start HTTP server (optional, just for general HTTP routes)
 }
 
 void loop() {
-  client.poll(); // keep connection alive
+  if (client.available()) {
+    client.poll();  // Keep the WebSocket connection alive
+  } else {
+    connectToWebSocketServer();  
+  }
 
-  int ldrValue = analogRead(ldrPin);   // Send sensor data (LDR reading)
+  int ldrValue = analogRead(ldrPin);  
   String message = String(ldrValue);
 
   Serial.println(message);
   client.send(message);
-  // MDNS.update(); // mDNS will automatically handle DNS resolution once started
   
-  // ws.textAll(message);  // Send LDR data to all connected WebSocket clients
-  delay(1); 
+  delay(1000); 
 }
-
-// void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-//   if (type == WS_EVT_CONNECT) {
-//     Serial.println("WebSocket client connected!");
-//   } else if (type == WS_EVT_DISCONNECT) {
-//     Serial.println("WebSocket client disconnected!");
-//   } else if (type == WS_EVT_DATA) {
-//     // Optionally, handle data received from WebSocket server
-//     String receivedMessage = String((char*)data);
-//     Serial.println("Received message: " + receivedMessage);
-//   }
-// }
